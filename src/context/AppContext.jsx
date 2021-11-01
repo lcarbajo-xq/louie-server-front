b957a6e8deb94e31da34da1cdd429ac4a19406c3
lcarbajo-xq <luis.carbajo.arguelles@gmail.om>
2021-11-01 17:55:43 +0100

    fix (Root) : Context return

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,10 +2,10 @@ import { createContext, useContext, useReducer } from 'react'
 
 const AppContext = createContext()
 
-export const AppContextProvider = ({ initialState, reducer, children }) => {
-  ;<AppContext.Provider value={useReducer(reducer, initialState)}>
+export const AppContextProvider = ({ initialState, reducer, children }) => (
+  <AppContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </AppContext.Provider>
-}
+)
 
 export const useAppContext = () => useContext(AppContext)
