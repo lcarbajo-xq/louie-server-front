e3339b8c099280cc4fa7ce83bdb99db1d96581f1
lcarbajo-xq <luis.carbajo.arguelles@gmail.om>
2021-11-06 01:30:14 +0100

    Fix (Root) : Dropdown 2

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -11,13 +11,15 @@ export const Dropdown = ({ children, config, id }) => {
   const [state, dispatch] = useAppContext()
 
   useEffect(() => {
+    return () => handleUnmountDropdown()
+  }, [])
+
+  useEffect(() => {
     if (state.activeDropdown === id) {
       setIsOpen(true)
     } else {
       setIsOpen(false)
     }
-
-    return () => handleUnmountDropdown()
   }, [state?.activeDropdown])
 
   const handleUnmountDropdown = () => {
