d4e98649699768a2ff8c6d377aef8da99c3fd528
lcarbajo-xq <luis.carbajo.arguelles@gmail.om>
2021-11-07 15:44:13 +0100

    fic (Root) : Active link routes

--- src/components/Header/NavLink.jsx
+++ src/components/Header/NavLink.jsx
@@ -1,7 +1,15 @@
-import { Link, useRoute } from 'wouter'
+import { useEffect, useState } from 'react'
+import { Link, useRoute, useLocation } from 'wouter'
 
 export const NavLink = ({ href, icon, title }) => {
-  const [isActive] = useRoute(href)
+  const [isActive, setIsActive] = useState(false)
+  const [location] = useLocation()
+
+  useEffect(() => {
+    if (location.includes(href)) setIsActive(true)
+    else setIsActive(false)
+  }, [location])
+
   return (
     <>
       <Link to={href}>
